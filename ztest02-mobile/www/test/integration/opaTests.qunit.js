07b5513e8022d83d29ab9c7a2c62a68fedb442f8
amagnontt <vmagnmor@emeal.nttdata.com>
2023-02-10 09:48:41 -0500

    Se agrega app ui5 a apache cordova

---
+++ ztest02-mobile/www/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/nttdata/ztest02ui5/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ ztest02-mobile/www/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comnttdata/ztest02-ui5/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
